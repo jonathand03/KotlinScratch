7a21746dc38c11d91e1b0b05c6d18c043855e007
Jonathan Drumond <jonathandrumond03@gmail.com>
2023-01-08 21:36:33 -0300

    Criando singup

--- java_estudos/src/SingUp/Singup.java
+++ java_estudos/src/SingUp/Singup.java
@@ -1,12 +1,23 @@
 package SingUp;
 
+import Client.Client;
+
+import java.util.Scanner;
+
 public class Singup {
     private String registerMessage          = "Bem - Vindo ao Centro de Cadastro";
     private String registerGetNameMessage   = "Por favor, digite seu nome: ";
     private String registerGetAgeMessage    = "Por favor, digite sua idade: ";
     private String regigsterGetAddress      = "Por favor, digite seu endereço";
     private String teste = "Teste";
-    public void register(){
 
+    public void register(){
+            System.out.println(this.registerMessage);
+            Scanner readKeyBoard = new Scanner(System.in);
+            System.out.println(this.registerGetNameMessage);
+            String getRegisterName = readKeyBoard.nextLine();
+            if(getRegisterName != null){
+                Client newClient = new Client();
+            }
     }
 }
